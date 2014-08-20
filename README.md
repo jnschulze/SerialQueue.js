9f10046397cf9031df8e87522acc1f70546e2a24
Jan N. Schulze <me@jns.io>
2014-08-20 18:08:58 +0200

    Add code

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ SerialQueue.js
 ==============
 
 Enables serial processing of enqueued items using an asynchronous worker function.
+
+```javascript
+
+var myWorker = function(item, next) {
+    // Process your item (FOO, BAR, etc.) here.
+    
+    // call next() when finished.
+    next();
+};
+
+var queue = new SerialQueue(myWorker);
+queue.push('FOO');
+queue.push('BAR');
+```
+
+All enqueued items are processed using the specified worker function which is expected to call 'next' when it has finished processing the respective item.
---
+++ src/SerialQueue.js
@@ -0,0 +1,68 @@
+;(function (name, definition) {
+    var theModule = definition(),
+    hasDefine = typeof define === 'function' && define.amd,
+    hasExports = typeof module !== 'undefined' && module.exports;
+
+    if(hasDefine) // AMD Module
+        define(theModule);
+    else if(hasExports) // Node.js Module
+        module.exports = theModule;
+    else
+        this[name] = theModule;
+})('SerialQueue', function() {
+
+    'use strict';
+
+    var Q = function(worker)
+    {
+        this._running = false;
+        this._worker  = worker || this._defaultWorker;
+        this._q = [];
+    };
+
+    Q.prototype = {
+        constructor: Q,
+
+        push: function(item)
+        {
+            this._q.push(item);
+            this._run();
+        },
+
+        _runNext: function(done)
+        {
+            var self = this;
+
+            var workerDone = function()
+            {
+                if(self._q.length)
+                {
+                    self._worker(self._q.shift(), workerDone);
+                }
+                else
+                {
+                    self._running = false;
+                    done();
+                }
+            }
+
+            workerDone();
+        },
+
+        _run: function()
+        {
+            var self = this;
+            if(!self._running)
+            {
+                self._running = true;
+                self._runNext(function()
+                {
+                    self._running = false;
+                });
+            }
+        }
+    };
+
+    return Q;
+ 
+});
